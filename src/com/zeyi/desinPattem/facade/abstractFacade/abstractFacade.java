bb0cf7e36e86fa715688d8aec0ce60e225cf2df3
yangsen13 <yangsen1@jd.com>
2016-11-30 10:01:05 +0800

    commit

--- src/com/zeyi/desinPattem/facade/abstractFacade/abstractFacade.java
+++ src/com/zeyi/desinPattem/facade/abstractFacade/abstractFacade.java
@@ -4,7 +4,7 @@ package com.zeyi.desinPattem.facade.abstractFacade;
  * Created by yangsen1 on 2016/11/30.
  */
 public abstract class AbstractFacade {
-    //为啥会在提交到github上修改文件名称呢
+    //为啥会在提交到github上
     public void method(){
     }
 }
