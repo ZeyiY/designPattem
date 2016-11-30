36052882955b9636cada9993d3e5edb4bdcb6979
yangsen13 <yangsen1@jd.com>
2016-11-30 11:10:04 +0800

    查找问题

--- src/com/zeyi/desinPattem/facade/abstractFacade/AbstractFacade.java
+++ src/com/zeyi/desinPattem/facade/abstractFacade/AbstractFacade.java
@@ -4,11 +4,6 @@ package com.zeyi.desinPattem.facade.abstractFacade;
  * Created by yangsen1 on 2016/11/30.
  */
 public abstract class AbstractFacade {
-<<<<<<< HEAD:src/com/zeyi/desinPattem/facade/abstractFacade/abstractFacade.java
-    //为啥会在提交到github上
-=======
-    //涓哄暐浼氬湪鎻愪氦鍒癵ithub涓婁慨鏀规枃浠跺悕绉板憿
->>>>>>> 3a6cc994dcdce9d1f4a4066a104a8a5844204b48:src/com/zeyi/desinPattem/facade/abstractFacade/AbstractFacade.java
     public void method(){
     }
 }
